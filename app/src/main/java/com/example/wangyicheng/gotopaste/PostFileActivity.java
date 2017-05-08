a945056413f1a9f35f41529634dd20e6271e120a
xiaokecheng <1400012911@pku.edu.cn>
2017-05-08 13:59:43 +0800

    add post msg and file activities

--- app/src/main/java/com/example/wangyicheng/gotopaste/PostFileActivity.java
+++ app/src/main/java/com/example/wangyicheng/gotopaste/PostFileActivity.java
@@ -1,15 +1,46 @@
 package com.example.wangyicheng.gotopaste;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 // This activity is responsible for pushing files
 // Shared files and private files included
 public class PostFileActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private ImageButton file_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_file);
+
+        file_button = (ImageButton) findViewById(R.id.file_button);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.back);
+        toolbar.inflateMenu(R.menu.post_toolbar_menu);
+
+        file_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            Log.i("uri", uri.toString());
+        }
     }
 }
